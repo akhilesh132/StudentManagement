3590a4cb06a3eed3e977494d990fe2f0b780d6ff
Akhilesh Kumar <rajakhil132@gmail.com>
2021-09-18 21:45:17 +0530

    Configuring source and target compatibility to 11

--- security/src/main/java/module-info.java
+++ security/src/main/java/module-info.java
@@ -1,2 +1,20 @@
-module com.akhilesh.hrms.security {
-}
+//module com.akhilesh.hrms.security {
+////    requires spring.data.jpa;
+////    requires spring.context;
+////    requires spring.beans;
+////    requires spring.security.core;
+////    requires spring.security.crypto;
+////    requires spring.security.config;
+////    requires spring.security.web;
+////    requires spring.web;
+////    requires spring.context.support;
+////    requires java.persistence;
+////    requires spring.data.commons;
+////    requires java.validation;
+////    requires org.apache.commons.lang3;
+////    requires jjwt;
+////
+////    exports com.akhilesh.hrms.security.beans;
+////    exports com.akhilesh.hrms.security.configs;
+//
+//}
